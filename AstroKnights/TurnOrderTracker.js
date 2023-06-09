d4940bb0c9ea5e89fa158e0297ee714c7fb86da4
NightB1ade <NightB1ade@users.noreply.github.com>
2023-06-09 09:01:48 +0100

    Astro Knights: Turn Order Tracker: Updates

--- AstroKnights/TurnOrderTracker.js
+++ AstroKnights/TurnOrderTracker.js
@@ -22,18 +22,16 @@ function NextTurn() {
 
 	document.getElementById("TurnOrderTracker").insertAdjacentHTML("beforeend",
 		'<div class="row mt-1 py-2 align-items-center '
-		+ (card == "Boss" ? "bg-danger-subtle" : "")
-		+ (card == "Hero 1" ? "bg-primary-subtle" : "")
-		+ (card == "Hero 2" ? "bg-secondary-subtle" : "")
+		+ (["Boss","Boss 1","Boss 2"].includes(card) ? "bg-danger-subtle" : "")
+		+ (["Hero 1","Hero 1/2"].includes(card) ? "bg-primary-subtle" : "")
+		+ (["Hero 2","Hero 3/4"].includes(card) ? "bg-secondary-subtle" : "")
 		+ (card == "Hero 3" ? "bg-success-subtle" : "")
 		+ (card == "Wild" ? "bg-info-subtle" : "")
-		+ (card == "Hero 1/2" ? "bg-primary-subtle" : "")
-		+ (card == "Hero 3/4" ? "bg-secondary-subtle" : "")
 		+ '">'
-			+ '<div class="col col-3">' + card + '</div>'
-			+ '<div class="col col-2"><button class="btn btn-secondary" type="button" onclick="ReturnToDrawDeck(this,\'' + card + '\',\'Top\')">&uArr;</button></div>'
-			+ '<div class="col col-2"><button class="btn btn-secondary" type="button" onclick="ReturnToDrawDeck(this,\'' + card + '\',\'Bottom\')">&dArr;</button></div>'
-			+ '<div class="col col-2"><button class="btn btn-secondary" type="button" onclick="ReturnToDrawDeck(this,\'' + card + '\',\'Shuffle\')">&#10227;</button></div>'
+			+ '<div class="col col-6 col-sm-4 col-md-2">' + card + '</div>'
+			+ '<div class="col col-2 col-md-1"><button class="btn btn-secondary" type="button" onclick="ReturnToDrawDeck(this,\'' + card + '\',\'Top\')">&uArr;</button></div>'
+			+ '<div class="col col-2 col-md-1"><button class="btn btn-secondary" type="button" onclick="ReturnToDrawDeck(this,\'' + card + '\',\'Bottom\')">&dArr;</button></div>'
+			+ '<div class="col col-2 col-md-1"><button class="btn btn-secondary" type="button" onclick="ReturnToDrawDeck(this,\'' + card + '\',\'Shuffle\')">&#10227;</button></div>'
 			+ '<div class="w-100"></div>'
 		+ '</div>'
 	);
@@ -71,21 +69,36 @@ function SetupModal_Save() {
 	var form = document.getElementById("SetupModal_Form");
 	var numHeroes = form.NumHeroes.value;
 
-	document.getElementById("btn_Setup").innerHTML = "Number of Heroes: " + numHeroes;
+	document.getElementById("SetupSettings").innerHTML = '<ul class="list-unstyled">'
+		+ '<li>Number of Heroes: ' + numHeroes + '</li>'
+		+ '<li>Boss Cards Numbered? ' + (form.IsBossCards_Numbered.checked ? "Yes" : "No") + '</li>'
+	+ '</ul>';
 	document.getElementById("TurnOrderSection").classList.remove("d-none");
 
 	if (numHeroes == 1) {
-		turnOrderDeck = ["Hero 1","Hero 1","Hero 1","Boss","Boss"];
+		turnOrderDeck = ["Hero 1","Hero 1","Hero 1"];
 	} else if (numHeroes == 2) {
-		turnOrderDeck = ["Hero 1","Hero 1","Hero 2","Hero 2","Boss","Boss"];
+		turnOrderDeck = ["Hero 1","Hero 1","Hero 2","Hero 2"];
 	} else if (numHeroes == 3) {
-		turnOrderDeck = ["Hero 1","Hero 2","Hero 3","Wild","Boss","Boss"];
+		turnOrderDeck = ["Hero 1","Hero 2","Hero 3","Wild"];
 	} else if (numHeroes == 4) {
-		turnOrderDeck = ["Hero 1/2","Hero 1/2","Hero 3/4","Hero 3/4","Boss","Boss"];
+		turnOrderDeck = ["Hero 1/2","Hero 1/2","Hero 3/4","Hero 3/4"];
+	}
+
+	if (form.IsBossCards_Numbered.checked) {
+		turnOrderDeck.push("Boss 1");
+		turnOrderDeck.push("Boss 2");
+	} else {
+		turnOrderDeck.push("Boss");
+		turnOrderDeck.push("Boss");
 	}
 
 	turnOrderDrawDeck = [...turnOrderDeck];
 	ShuffleArray(turnOrderDrawDeck);
+
+	document.getElementById("TurnOrderTracker").innerHTML = "";
+	document.getElementById("btn_NextTurn").disabled = false;
+	document.getElementById("btn_NewRound").disabled = true;
 
 	bootstrap.Modal.getInstance(document.getElementById("SetupModal")).hide();
 }
