73ac5ec090125943be06b59b151f459e31a49fd3
NightB1ade <NightB1ade@users.noreply.github.com>
2023-06-09 11:04:49 +0100

    Astro Knights: Turn Order Tracker: Hero -> Knight

--- AstroKnights/TurnOrderTracker.js
+++ AstroKnights/TurnOrderTracker.js
@@ -23,9 +23,9 @@ function NextTurn() {
 	document.getElementById("TurnOrderTracker").insertAdjacentHTML("beforeend",
 		'<div class="row mt-1 py-2 align-items-center '
 		+ (["Boss","Boss 1","Boss 2"].includes(card) ? "bg-danger-subtle" : "")
-		+ (["Hero 1","Hero 1/2"].includes(card) ? "bg-primary-subtle" : "")
-		+ (["Hero 2","Hero 3/4"].includes(card) ? "bg-secondary-subtle" : "")
-		+ (card == "Hero 3" ? "bg-success-subtle" : "")
+		+ (["Knight 1","Knight 1/2"].includes(card) ? "bg-primary-subtle" : "")
+		+ (["Knight 2","Knight 3/4"].includes(card) ? "bg-secondary-subtle" : "")
+		+ (card == "Knight 3" ? "bg-success-subtle" : "")
 		+ (card == "Wild" ? "bg-info-subtle" : "")
 		+ '">'
 			+ '<div class="col col-6 col-sm-4 col-md-2">' + card + '</div>'
@@ -67,22 +67,22 @@ function ReturnToDrawDeck(btn,card,position) {
 
 function SetupModal_Save() {
 	var form = document.getElementById("SetupModal_Form");
-	var numHeroes = form.NumHeroes.value;
+	var numKnights = form.NumKnights.value;
 
 	document.getElementById("SetupSettings").innerHTML = '<ul class="list-unstyled">'
-		+ '<li>Number of Heroes: ' + numHeroes + '</li>'
+		+ '<li>Number of Knights: ' + numKnights + '</li>'
 		+ '<li>Boss Cards Numbered? ' + (form.IsBossCards_Numbered.checked ? "Yes" : "No") + '</li>'
 	+ '</ul>';
 	document.getElementById("TurnOrderSection").classList.remove("d-none");
 
-	if (numHeroes == 1) {
-		turnOrderDeck = ["Hero 1","Hero 1","Hero 1"];
-	} else if (numHeroes == 2) {
-		turnOrderDeck = ["Hero 1","Hero 1","Hero 2","Hero 2"];
-	} else if (numHeroes == 3) {
-		turnOrderDeck = ["Hero 1","Hero 2","Hero 3","Wild"];
-	} else if (numHeroes == 4) {
-		turnOrderDeck = ["Hero 1/2","Hero 1/2","Hero 3/4","Hero 3/4"];
+	if (numKnights == 1) {
+		turnOrderDeck = ["Knight 1","Knight 1","Knight 1"];
+	} else if (numKnights == 2) {
+		turnOrderDeck = ["Knight 1","Knight 1","Knight 2","Knight 2"];
+	} else if (numKnights == 3) {
+		turnOrderDeck = ["Knight 1","Knight 2","Knight 3","Wild"];
+	} else if (numKnights == 4) {
+		turnOrderDeck = ["Knight 1/2","Knight 1/2","Knight 3/4","Knight 3/4"];
 	}
 
 	if (form.IsBossCards_Numbered.checked) {
