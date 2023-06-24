a7141c9d2e826d04048e10a6c5ca3ad8d0d5fe88
NightB1ade <NightB1ade@users.noreply.github.com>
2023-06-24 13:56:25 +0100

    Tokaido: Clean Up Javascript

--- Tokaido/index.js
+++ Tokaido/index.js
@@ -1,6 +1,5 @@
 function CharacterFilter(expansion) {
 	var table = document.getElementById("CharacterTable");
-	var display = document.getElementById("cbx_Characters_" + expansion).checked;
 
 	Array.from(table.getElementsByClassName("Expansion_" + expansion)).forEach((item, i) => {
 		item.classList.toggle("d-none");
